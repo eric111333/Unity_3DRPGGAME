b7cf49125e3e8bf21496b55c5486e90c1f6b4ec9
eric111333 <67904763+eric111333@users.noreply.github.com>
2020-08-26 21:32:05 +0800

    0826 進度條

--- 3DRPGGame/Assets/Script/MenuManager.cs
+++ 3DRPGGame/Assets/Script/MenuManager.cs
@@ -38,13 +38,21 @@ public class MenuManager : MonoBehaviour
         ao.allowSceneActivation = false;                           //載入資訊.允許自動載入 = 否 不允許自動載入
 
         //當載入訊息.完成 為false -  尚未載入完程時執行
-        while(!ao.isDone)                                          //(ao.isDone == false)
+        while (!ao.isDone)                                          //(ao.isDone == false)
         {
             //progress 載入場景的進度為0-1.如果設定allow為false會卡在0.9
             //ToString("F小數點位數"):小數點2位F2 .小數點0位F0 
-            textLoading.text = "載入進度" + (ao.progress/0.9f*100).ToString("F1") + "％";//載入文字 = "載入進度" + ao.進度*100 + "％"
-            imgLoading.fillAmount = ao.progress/0.9f;
+            textLoading.text = "載入進度" + (ao.progress / 0.9f * 100).ToString("F1") + "％";//載入文字 = "載入進度" + ao.進度*100 + "％"
+            imgLoading.fillAmount = ao.progress / 0.9f;
             yield return null;
+
+            if (ao.progress == 0.9f)         //如果 ao.進度=0.9                   
+            {
+                textTip.enabled = true;      //提是文字.啟動 = 是 - 顯示提是文字
+                if (Input.anyKeyDown)
+                    ao.allowSceneActivation = true; //如果按下任意鍵 允許進入
+
+            }
 
         }
 
